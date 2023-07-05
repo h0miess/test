93dd085f88ee0554c4b21046fb3e502121de8ff8
Egor Karabanov <e.karabanov@vibelab.ru>
2023-07-05 22:48:39 +0300

    out 123567

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
         System.out.println("git");
+        System.out.println("1234567");
     }
 }
