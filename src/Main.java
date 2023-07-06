ab8b46d519d7b9cee644a57e61e3701ee78956b4
Egor Karabanov <e.karabanov@vibelab.ru>
2023-07-06 13:26:41 +0300

    Add good bye

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!!!");
         System.out.println("git");
         System.out.println("1234567");
+        System.out.println("good bye!");
     }
 }
