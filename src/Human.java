b8523b9870e825c0f797787e563af1edc8a08520
Egor Karabanov <e.karabanov@vibelab.ru>
2023-07-06 21:24:25 +0300

    refactor human class

--- src/Human.java
+++ src/Human.java
@@ -1,10 +1,10 @@
 public class Human {
     private int age;
-     public Human(int age) {
-         this.age = age;
-     }
-
      public int getAge() {
          return this.age;
+     }
+
+     public void setAge(int age) {
+         this.age = age;
      }
 }
