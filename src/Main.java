79555e65614cc0e72be07c1e38243634d836a29a
Egor Karabanov <e.karabanov@vibelab.ru>
2023-07-06 14:32:58 +0300

    add try ssh

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("git");
         System.out.println("1234567");
         System.out.println("good bye!");
+        System.out.println("try ssh");
     }
 }
