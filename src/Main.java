3c1fee1da6e087a95f88dc438dc8ae28ad027e07
Egor Karabanov <e.karabanov@vibelab.ru>
2023-07-06 18:15:43 +0300

    delete numbers out

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
         System.out.println("git");
-        System.out.println("1234567");
     }
 }
