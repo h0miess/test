4b7c9fcab5f233f9dba569c75f97254a58a30219
Egor Karabanov <e.karabanov@vibelab.ru>
2023-07-06 14:50:24 +0300

    reset main file to out 1234567

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,5 @@ public class Main {
         System.out.println("Hello world!!!");
         System.out.println("git");
         System.out.println("1234567");
-        System.out.println("good bye!");
-        System.out.println("try ssh");
     }
 }
